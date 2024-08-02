2270e6d2ecf3cd24bf1afb89ece50410564da7be
Sanjaiprasath <161956373+Sanjaiprasath68@users.noreply.github.com>
2024-08-02 20:24:48 +0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Home from './Home';
 import BillPage from './BillPage';
 import Items from './components/items/Items';
 import LoginPage from './LoginPage';
+import Protected from './Protected';
+
 
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const App = () => {
+  const [isSignedIn, setIsSignedIn] = useState(null);
+
+  const signin = () => {
+    setIsSignedIn(true)
+  }
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/home' element={<Home />} />
-        <Route path='/' element={<LoginPage />} />
-        <Route path='/billpage' element={<BillPage />} />
-        <Route path='/items' element={<Items />} />
+        <Route
+          path='/home'
+          element={
+            <Protected isSignedIn={isSignedIn}>
+              <Home />
+            </Protected>} />
+        <Route path='/' element={<LoginPage signin={signin}/>} />
+        <Route
+         path='/billpage'
+          element={
+            <Protected isSignedIn={isSignedIn}>
+        <BillPage />
+        </Protected> }/>
+        <Route
+         path='/items'
+          element={
+            <Protected isSignedIn={isSignedIn}>
+          <Items />
+          </Protected>} />
       </Routes>
     </BrowserRouter>
   );
